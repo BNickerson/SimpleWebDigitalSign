e965f2edac66adbfa639f855a1ce02a45fe8f813
Blake <blake.r.nickerson@gmail.com>
2020-02-24 22:01:51 -0600

    Update main.js

--- docs/example/js/main.js
+++ docs/example/js/main.js
@@ -6,13 +6,13 @@ document.addEventListener('DOMContentLoaded', async function() {
             media: 'https://www.setaswall.com/wp-content/uploads/2017/10/Vermillion-Lake-Stars-Qhd-Wallpaper-1080x1920.jpg',
             duration: 10,
             type: 'image',
-            transition: 'tada' // optional
+            transition: 'fadeInDown' // optional
         },
         {
             media: 'https://wallpaperplay.com/walls/full/0/6/3/13650.jpg',
             duration: 10,
             type: 'image',
-            transition: 'tada' // optional
+            transition: 'fadeInDown' // optional
         }
     ]
 
